f8ec0c084f00c4d5cdb1c24cd36535549cf50c1f
jconnollyweb <johnc1983@hotmail.co.uk>
2024-04-10 17:54:18 +0100

    added contact page

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,16 +4,25 @@ import { Link } from 'react-router-dom'
 function Header() {
     return (
         <div className={styles.headerBody}>
+            <div className={styles.decOne}>
+                <h5> &lt;JC&gt;</h5>
+            </div>
            <div className={styles.logo}>
-           <h1>John Connolly's Portfolio </h1>
+           <h3 className={styles.h3}> Hi,</h3>
+           <h1>I'm John Connolly</h1>
+           <p>Recently graduated Software Developer with a passion for creating attractive and <br/>user friendly websites.  </p>
          </div>
+         <div className={styles.decTwo}>
+               <h5>&lt;/&gt;</h5>
+           </div>
          <div className={styles.links}>
             <ul>
-                <li>ABOUT ME</li>
+                <li><a href="/aboutme">ABOUT ME</a></li>
                 <li>
                 <a href="/CV">CV</a>
                 </li>
-                <li>CONTACT ME</li>
+                <li><a href="/contact">CONTACT ME</a></li>
+                <li><a href="/portfolio">PROJECTS</a></li>
             </ul>
 
          </div>
