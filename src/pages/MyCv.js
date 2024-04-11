6c1e69f5ee0cbbf9d258bd4e70f67ec5f9c78c7a
jconnollyweb <johnc1983@hotmail.co.uk>
2024-04-11 15:22:18 +0100

    new cv

--- src/pages/MyCv.js
+++ src/pages/MyCv.js
@@ -3,7 +3,7 @@
 function MyCv() {
     return (
         <div className="cv-container" style={{ height: "100vh", paddingLeft: "7%" }}> 
-            <iframe src="https://1drv.ms/w/c/7bffc89783e92f2e/IQPOACsDESSSQaWXUKO045uaAbSUyasFFMQBc5XGEYIyoF0" width="90%" height="90%" frameborder="0" scrolling="no"></iframe>
+            <iframe src="https://1drv.ms/w/c/7bffc89783e92f2e/IQMGHj6d1MuIRLNr8e012ty2AYAKAMcFUbh2isFzYVqo-Ug?em=2" width="90%" height="100%" frameborder="0">This is an embedded <a target="_blank" href="https://office.com">Microsoft Office</a> document, powered by <a target="_blank" href="https://office.com/webapps">Office</a>.</iframe>
         </div>
     )
 }
